660a9c031ff3e2bb649b3ece4c2801099c2b16e9
Info-Vigneshwaran <info.vigneshwaran@gmail.com>
2022-03-05 19:04:43 +0530

    class component function added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './style.css';
 
-export default function App() {
-  return (
-    <div>
-      <h1>Hello StacsdsdkBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.setstate = {
+      inc: 1,
+      todosdata: [],
+      loading: false,
+    };
+
+    this.incrementMe = this.incrementMe.bind(this);
+    this.todoApicall = this.todoApicall.bind(this);
+  }
+
+  componentDidMount() {
+    this.setState({
+      loading: false,
+    });
+  }
+
+  todoApicall = () => {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+      .then((response) => response.json)
+      .then((data) => {
+        this.setState(
+          {
+            todosdata: data,
+            loading: true,
+          },
+          () => {
+            console.log(this.state.loading);
+          }
+        );
+      });
+  };
+
+  incrementMe = () => {};
+  render() {
+    return (
+      <>
+        <div>
+          <button onClick={this.todoApicall}>Call API</button>
+          <button onClick={this.incrementMe}>Increment</button>
+        </div>
+        <p>{this.state.loading}</p>
+      </>
+    );
+  }
 }
+export default App;
