fb5beb4e96a5963bc4855fecf71cfab1405ddd45
Info-Vigneshwaran <info.vigneshwaran@gmail.com>
2022-03-18 00:09:26 -0700

    1 file modified

--- src/Todo.js
+++ src/Todo.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import data from './data';
 
 export default function Todo() {
@@ -8,7 +8,15 @@ export default function Todo() {
     e.preventDefault();
     setName(e.target.value);
   };
-
+  useEffect(() => {}, [lists]);
+  const handleDone = (id) => {
+    let new_list = lists.map((list) => {
+      console.log(!list.done);
+      return list.id == id ? { ...list, done: !list.done } : { ...list };
+    });
+    setLists(new_list);
+    console.log(new_list);
+  };
   const handleClick = () => {
     if (name != '') {
       let new_item = { id: lists.length + 1, name: name, done: false };
@@ -19,25 +27,19 @@ export default function Todo() {
   };
 
   const handleRemove = (id) => {
-    let new_list = lists.filter(function (list) {
+    let new_list = lists.filter((list) => {
       return list.id !== id;
     });
     setLists(new_list);
   };
 
-  const handleDone = (id) => {
-    let new_list = lists.filter(function (list) {
-      return list.id == id ? { ...list, id: !list.done } : { ...list };
-    });
-    setLists(new_list);
-  };
   return (
     <div>
       <p>test</p>
       <input type="text" onChange={handleChange} value={name} />
       <input type="button" onClick={handleClick} value="Add" />
-      {lists.map((list) => (
-        <div key={list.id}>
+      {lists.map((list, index) => (
+        <div key={index}>
           <input
             type="checkbox"
             onChange={() => handleDone(list.id)}
