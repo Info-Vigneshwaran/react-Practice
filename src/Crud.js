6b8a589aada7b0a5b61f78573eacde804422ec67
Info-Vigneshwaran <info.vigneshwaran@gmail.com>
2022-03-18 18:42:42 +0530

    1 file modified

--- src/Crud.js
+++ src/Crud.js
@@ -2,33 +2,25 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 export default function Crud() {
   const [users, setUsers] = useState([]);
-  const usersCheck = useEffect(() => {
-    axios
-      .get('https://6018e358971d850017a4058a.mockapi.io/api/v1/Users')
-      .then((res) => {
-        setUsers(res.data);
-        console.log(res.data);
-      });
-  }, []);
 
   return (
     <>
       <table>
-        <thead>
-          <tr>
-            <td>Id</td>
-            <td>Name</td>
-            <td>Action</td>
-          </tr>
-        </thead>
-        {users.map((user) => {
-          <tr>
-            <td>{user.id}</td>
-            <td>{user.name}</td>
-            <td>
-              <input type="button" Value="Edit" />
-            </td>
-          </tr>;
+        <tr>
+          <th>Name</th>
+          <th>Age</th>
+          <th>Gender</th>
+        </tr>
+        {users.map((list, key) => {
+          return (
+            <tr key={key}>
+              <td>{list.id}</td>
+              <td>{list.name}</td>
+              <td>
+                <input type="button" />
+              </td>
+            </tr>
+          );
         })}
       </table>
     </>
